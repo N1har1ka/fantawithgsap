3c7d36dd8a2ad35117eea4430b6cc10134e1a552
N1har1ka <134865638+N1har1ka@users.noreply.github.com>
2024-11-01 18:19:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,69 +1,73 @@
-var tl=gsap.timeline({scrollTrigger:{
-    trigger:".two",
-    start:"0% 95%",
-    end:"50% 50%",
-    scrub:true,
-    markers:false
-}});
+var tl = gsap.timeline({scrollTrigger:{
+    trigger: ".two",
+    start: "0% 95%",
+    end: "60% 50%",
+    scrub: true,
+    // markers: true,
+}})
+
 tl.to("#fanta",{
-    top:"112%",
-    left:"3%",
-},'orange')
+    top: "120%",
+    left: "0%"
+}, 'orange')
 tl.to("#orange-cut",{
-    top:"155%",
-    left:"25%"
-},'orange')
+    top:"160%",
+    left: "23%"
+}, 'orange')
 tl.to("#orange",{
-    width:"15%",
-    top:"165%",
-    right:"10%"
-},'orange')
+    width: "15%",
+    top:"160%",
+    right: "10%"
+}, 'orange')
 tl.to("#leaf",{
-    top:"100%",
-    rotate:"130deg",
-    left:"78%"
-},'orange')
+    top:"110%",
+    rotate: "130deg",
+    left: "70%"
+}, 'orange')
 tl.to("#leaf2",{
     top:"110%",
-    rotate:"130deg",
-    left:"0%"
-},'orange')
+    rotate: "130deg",
+    left: "0%"
+}, 'orange')
 
-var tl2=gsap.timeline({scrollTrigger:{
-    trigger:".three",
-    start:"20% 95%",
-    end:"40% 50%",
-    scrub:true,
-    markers:false
-}});
+
+var tl2 = gsap.timeline({scrollTrigger:{
+    trigger: ".three",
+    start: "0% 95%",
+    end: "20% 50%",
+    scrub: true,
+    // markers: true,
+}})
+
 tl2.from(".lemon1",{
-    rotate:"-90deg",
-    left:"-80%",
-    top:"60%",
-},"ca")
+    rotate: "-90deg",
+    left: "-100%",
+    top: "110%"
+}, 'ca')
 tl2.from("#cocacola",{
-    rotate:"-90deg",
-    left:"-60%",
-    top:"50%",
-},"ca")
+    rotate: "-90deg",
+    top: "160%",
+    left: "-100%",
+}, 'ca')
 
 tl2.from(".lemon2",{
-    rotate:"90deg",
-    left:"80%",
-    top:"60%",
-},"ca")
+    rotate: "90deg",
+    left: "100%",
+    top: "110%"
+}, 'ca')
 tl2.from("#pepsi",{
-    rotate:"90deg",
-    left:"60%",
-    top:"50%",
-},"ca")
+    rotate: "90deg",
+    top: "100%",
+    left: "100%",
+}, 'ca')
+
 tl2.to("#orange-cut",{
     width:"18%",
-    left:"41%",
-    top:"205%"
-},"ca")
+    left: "41%",
+    top: "204%"
+}, 'ca')
 tl2.to("#fanta",{
-    width:"32%",
-    left:"34%",
-    top:"212%"
-},"ca")
+    width:"35%",
+    top: "210%",
+    left: "32.5%",
+}, 'ca')
